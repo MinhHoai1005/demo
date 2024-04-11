d2099ebdf36a2e2db5dc86131a29551403b23ae8
hoaivm <hoaivm@ghn.vn>
2024-04-11 14:45:07 +0700

    splash screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import SplasScreen from './src/screens/SplashScreen'
 import AuthNavigator from './src/navigators/AuthNavigator'
@@ -10,18 +10,20 @@ const App = () => {
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-
-
+      setIsShowSplash(false);
     }, 1500)
 
     return clearTimeout(timeout)
   }, [])
-  return (
-    isShowSplash ? <SplasScreen /> :
-      <NavigationContainer>
-        <AuthNavigator />
-      </NavigationContainer>
-  )
+  return <>
+    <StatusBar barStyle='dark-content' translucent backgroundColor='transparent' />
+    {
+      isShowSplash ? <SplasScreen /> :
+        <NavigationContainer>
+          <AuthNavigator />
+        </NavigationContainer>
+    }
+  </>
 }
 
 export default App
--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
+import { ActivityIndicator, Image, ImageBackground } from 'react-native'
 import React from 'react'
+import { appInfo } from '../constants/appInfo'
+import { SpaceComponent } from '../components'
+import { appColor } from '../constants/appColors'
 
 const SplasScreen = () => {
   return (
-    <View>
-      <Text>SplasScreen</Text>
-    </View>
+    <ImageBackground source={require('../assets/images/splash-image.jpeg')}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      imageStyle={{
+        flex: 1,
+      }}
+    >
+      <Image source={require('../assets/images/home-logo.jpeg')}
+        style={{
+          width: appInfo.sizes.WIDTH * 0.8,
+          resizeMode: 'contain',
+        }}
+      />
+      <SpaceComponent height={16} />
+      <ActivityIndicator color={appColor.gray} size={22} />
+    </ImageBackground>
   )
 }
 
